796f72ad33c75da1c4095f7d2256022b3a67113f
AndyIsraelYanez <camilo.casas200@gmail.com>
2022-10-01 00:08:20 -0500

    funcion suma html mas js

---
+++ javaScript.js
@@ -0,0 +1,11 @@
+
+
+
+function sumar(){
+    var n1 = parseFloat(document.MyForm.num1.value);
+    var n2 = parseFloat(document.MyForm.num2.value);
+    document.MyForm.resultado.value = n1 + n2;
+}
+
+
+
